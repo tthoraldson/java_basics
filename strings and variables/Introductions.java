c54934ee7790f7caef0e099b2759020beffa64b2
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-01-06 21:54:13 -0600

    strings and vars

---
+++ strings and variables/Introductions.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+ 
+public class Introductions {
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+      
+        String firstName = "Theresa";
+        console.printf("Hello, World! \n");
+        console.printf("My name is %s\n", firstName); 
+  }
+}
